493a1ecc24560b3cd56fd2edff5b3231225d6de3
Yu-Cheng,Chen <tigernaxo@gmail.com>
2022-12-13 14:27:43 +0800

    tmp

--- script.sh
+++ script.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-if [ "`git status -s`" ]
+# git checkout --orphan gh-pages
+# git reset --hard
+# git commit --allow-empty -m "Initializing gh-pages branch"
+# git push origin gh-pages
+# git checkout main
+
+FILE="run_publish.sh"
+cat > $FILE <<- EOF
+#!/bin/bash
+if [ "\`git status -s\`" ]
 then
-	echo "The working directory is dirty. Please commit any pending changes."
-	exit 1;
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
 fi
 
 echo "Deleting old publication"
@@ -25,4 +34,6 @@ cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)
 
 #echo "Pushing to github"
 git push --all
-
+EOF
+chmod a+x ${FILE}
+./${FILE}
